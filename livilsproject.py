5fe9c9dec3fdc304c24ebeadc74114d7d9ec8d7d
elsadiah <101385293+elsadiah@users.noreply.github.com>
2022-03-11 15:06:14 +0700

    Add files via upload

---
+++ livilsproject.py
@@ -0,0 +1,35 @@
+print("Sugeng Rawuh ing livils cinema")
+print("")
+print("kenyamanan sampeyan nomor siji")
+print("-------------------------------")
+
+nama = input("Jeneng pelanggan : ")
+print("                         ")
+print("===== Nama film =====")
+print("1. Layangan pedot")
+print("2. Ono opo karo katresnan")
+print("3. Kkn desa penari")
+print("4. Pengabdi setan")
+print("5. Mr.bean")
+
+while(True):
+    nama_film = int(input("Masukan nomor film pilihan : "))
+    if nama_film== 1:
+         harga = 25000
+         break
+    elif nama_film==2 :
+         harga = 25000
+         break
+    elif nama_film==3 :
+         harga = 25000
+         break
+    elif nama_film==4 :
+         harga = 25000
+         break
+    elif nama_film==5 :
+         harga = 25000
+         break
+    else :
+        print("=====film kosong=====")
+
+print = input("Masukan jumlah pembelian : ")
